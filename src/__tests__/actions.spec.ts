7e6a91f312ebf137a221d7dcde046329b7a18fbf
Alan Soares <alanrsoars@gmail.com>
2018-10-12 00:02:10 +1300

    finish setup

--- src/__tests__/actions.spec.ts
+++ src/__tests__/actions.spec.ts
@@ -1,11 +1,29 @@
-import { createAction } from "../actions";
+import { createAction, createAsyncActions } from "../actions";
 
 describe("Actions", () => {
   describe("createAction", () => {
-    const actionCreator = createAction<string>("SAY_HELLO");
+    it("should create an actionCreator function", () => {
+      const actionCreator = createAction<string>("SAY_HELLO");
 
-    const action = actionCreator("World");
+      const action = actionCreator("World");
 
-    expect(action).toEqual({ type: "SAY_HELLO", payload: "World" });
+      expect(action).toEqual({ type: "SAY_HELLO", payload: "World" });
+    });
+  });
+
+  describe("createAsyncActions", () => {
+    it("should create a group of actions to handle async flows", () => {
+      interface Movie {
+        id: string;
+        title: string;
+      }
+
+      const fetchMovies = createAsyncActions<void, Movie[]>("FETCH", "MOVIES");
+
+      expect(fetchMovies()).toEqual({ type: "MOVIES/FETCH" });
+      expect(fetchMovies.request.type).toBe("MOVIES/FETCH_REQUEST");
+      expect(fetchMovies.success.type).toBe("MOVIES/FETCH_SUCCESS");
+      expect(fetchMovies.failure.type).toBe("MOVIES/FETCH_FAILURE");
+    });
   });
 });
