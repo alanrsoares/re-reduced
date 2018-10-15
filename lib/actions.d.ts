bd77c95d9d42b3f90b5dfd44365333a8bcb4ab51
Alan Soares <alanrsoars@gmail.com>
2018-10-15 23:43:45 +1300

    ignore lib

--- lib/actions.d.ts
+++
@@ -1,3 +0,0 @@
-import { ActionCreator, AsyncActions } from "./core";
-export declare function createAction<TPayload>(type: string): ActionCreator<TPayload>;
-export declare function createAsyncActions<TRun, TSuccess>(type: string, domain: string): AsyncActions<TRun, TSuccess>;
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./core";
-export * from "./reducers";
-export * from "./actions";
-export * from "./sagas";
