06b851b4ffaf56469b2365d56c90f744f3a72fba
Alan Soares <alanrsoars@gmail.com>
2018-10-23 18:36:57 +1300

    fix connectWithActions types

--- src/react-redux.ts
+++ src/react-redux.ts
@@ -1,10 +1,10 @@
 import { connect } from "react-redux";
 import { compose, Dispatch } from "redux";
 
-import { ActionCreator } from "./core";
+import { ActionCreator, ActionHandlerMap } from "./core";
 import { transformTree, Tree } from "./helpers/objects";
 
-type Dispatcher<T = any> = (payload: T) => void;
+export type Dispatcher<T = any> = (payload: T) => void;
 
 const toDispatcher = (dispatch: Dispatch) => <T>(action: ActionCreator<T>) =>
   compose<Dispatcher<T>>(
@@ -12,13 +12,13 @@ const toDispatcher = (dispatch: Dispatch) => <T>(action: ActionCreator<T>) =>
     action
   );
 
-export const connectWithActions = <T extends Tree<ActionCreator>>(
+export const connectWithActions = <T extends Tree<ActionCreator<any>>>(
   actions: T
 ) => <TState, TProps>(mapStateToProps: (state: TState) => TProps) => {
   const mapDisptachToProps = (dispatch: Dispatch) => ({
     actions: transformTree<ActionCreator, Dispatcher>({
       transformValue: toDispatcher(dispatch)
-    })(actions) as typeof actions
+    })(actions) as Tree<ActionCreator<any>>
   });
 
   return connect(
