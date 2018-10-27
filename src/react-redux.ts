06f9a17c5fcdc82b98d18c5497bb71103c1c416c
Alan Soares <alanrsoars@gmail.com>
2018-10-27 18:44:37 +1300

    add default mapStateToProps

--- src/react-redux.ts
+++ src/react-redux.ts
@@ -12,13 +12,18 @@ const toDispatcher = (dispatch: Dispatch) => <T>(action: ActionCreator<T>) =>
     action
   );
 
-type StateToProps<TState, TProps> = (state: TState) => TProps;
+type StateToProps<TState, TProps extends object = {}> = (
+  state: TState
+) => TProps;
 
 export function connectWithActions<
   TActions extends Tree<ActionCreator<any>>,
   TState,
-  TProps
->(actions: TActions, mapStateToProps: StateToProps<TState, TProps>) {
+  TProps extends object = {}
+>(
+  actions: TActions,
+  mapStateToProps: StateToProps<TState, TProps> = (_: TState) => ({} as TProps)
+) {
   const mapDisptachToProps = (dispatch: Dispatch) => ({
     actions: transformTree<ActionCreator, Dispatcher>({
       transformValue: toDispatcher(dispatch)
