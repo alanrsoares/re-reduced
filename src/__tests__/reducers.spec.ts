dd33d23fe41a2a802a89f6b53af213158d1e66b4
Alan Soares <alanrsoars@gmail.com>
2018-11-02 11:18:26 +1300

    add basic tests for handleActions

---
+++ src/__tests__/reducers.spec.ts
@@ -0,0 +1,29 @@
+import { number } from "prop-types";
+import { createAction, createAsyncActions } from "../actions";
+import { handleActions } from "../reducers";
+
+describe("Reducers", () => {
+  describe("handleActions", () => {
+    it("should create a reducer that's able to reduce the actions assigned to it", () => {
+      const actions = {
+        adjust: createAction<number>("ADJUST"),
+        decrement: createAction("DECREMENT"),
+        increment: createAction("INCREMENT")
+      };
+
+      const reducer = handleActions<number>(
+        [
+          actions.increment.reduce(state => state + 1),
+          actions.decrement.reduce(state => state - 1),
+          actions.adjust.reduce((state, payload) => state + payload)
+        ],
+        0
+      );
+
+      expect(reducer(0, actions.increment())).toBe(1);
+      expect(reducer(0, actions.decrement())).toBe(-1);
+      expect(reducer(10, actions.adjust(-5))).toBe(5);
+      expect(reducer(10, actions.adjust(5))).toBe(15);
+    });
+  });
+});
