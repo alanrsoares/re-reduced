56589bf27f0dac1ce255b4cd0b2f312a1de94733
Alan Soares <alanrsoars@gmail.com>
2018-11-14 12:10:05 +1300

    add more tests and test coverage

--- src/__tests__/reducers.spec.ts
+++ src/__tests__/reducers.spec.ts
@@ -1,5 +1,6 @@
-import { createAction } from "../actions";
-import { handleActions } from "../reducers";
+import { createAction, createAsyncAction } from "../actions";
+import { handleActions, createReducer, reducerConfig } from "../reducers";
+import { AsyncAction } from "../core";
 
 describe("Reducers", () => {
   describe("handleActions", () => {
@@ -23,6 +24,33 @@ describe("Reducers", () => {
       expect(reducer(0, actions.decrement())).toBe(-1);
       expect(reducer(10, actions.adjust(-5))).toBe(5);
       expect(reducer(10, actions.adjust(5))).toBe(15);
+    });
+  });
+
+  describe("createReducer", () => {
+    it("should create a reducer factory from a specification", () => {
+      const myActions = {
+        fetch: createAsyncAction<void, any[]>("FETCH_SOME_DATA_ASYNC")
+      };
+
+      const asyncActionMonitorReducerFactory = createReducer<
+        AsyncAction<any, any>,
+        boolean
+      >(
+        ({ actions }) => ({
+          [actions.request.type]: () => true,
+          [actions.success.type]: () => false,
+          [actions.failure.type]: () => false
+        }),
+        false
+      );
+
+      const config = reducerConfig({ actions: myActions.fetch });
+      const reducer = asyncActionMonitorReducerFactory(config);
+
+      expect(reducer(false, myActions.fetch.request())).toBe(true);
+      expect(reducer(true, myActions.fetch.success())).toBe(false);
+      expect(reducer(true, myActions.fetch.failure())).toBe(false);
     });
   });
 });
