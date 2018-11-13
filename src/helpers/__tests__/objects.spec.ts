9bd98f434cf8d4ec13116a769a1169dfa244100f
Alan Soares <alanrsoars@gmail.com>
2018-11-14 10:11:23 +1300

    add basic test case for helpers: objects.transformTree

---
+++ src/helpers/__tests__/objects.spec.ts
@@ -0,0 +1,40 @@
+import { transformTree } from "../objects";
+
+describe("Helpers - Objects", () => {
+  describe("transformTree", () => {
+    it("should apply a recursive transform function to nodes in a tree-like object", () => {
+      const input = {
+        a: {
+          a1: {
+            a1_1: "a1_1"
+          }
+        },
+        b: {
+          b1: {
+            b1_1: "b1_1"
+          }
+        },
+        c: "c"
+      };
+
+      const expected = {
+        a: {
+          a1: {
+            a1_1: "A1_1"
+          }
+        },
+        b: {
+          b1: {
+            b1_1: "B1_1"
+          }
+        },
+        c: "C"
+      };
+
+      const transform = (x: string) => x.toUpperCase();
+      const actual = transformTree(transform, input);
+
+      expect(actual).toEqual(expected);
+    });
+  });
+});
