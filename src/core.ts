bc8db59e3d5e001ecbfd8ee6809089b1429d1c92
Alan Soares <alanrsoars@gmail.com>
2018-11-14 10:25:01 +1300

    more jsdoc

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,6 @@
+/**
+ * A Flux-Standard-Action (FSA)
+ */
 export interface Action<T = void, TMeta = any> {
   type: string;
   payload: T;
@@ -5,6 +8,9 @@ export interface Action<T = void, TMeta = any> {
   error?: boolean;
 }
 
+/**
+ * A composite Action to handle async workflows with nested actions for request, success and failure
+ */
 export interface AsyncAction<TRun, TSuccess, TError = Error>
   extends ActionCreator<TRun> {
   request: ActionCreator;
@@ -12,20 +18,32 @@ export interface AsyncAction<TRun, TSuccess, TError = Error>
   failure: ActionCreator<TError>;
 }
 
+/**
+ * A reducer-like function that is handled by a specific action
+ */
 export type ActionReducer<TState, TPayload> = (
   state: TState,
   payload: TPayload
 ) => TState;
 
+/**
+ * A map-like structure where values are action-reducers
+ */
 export interface ActionReducerMap<TState> {
   [key: string]: ActionReducer<TState, any>;
 }
 
+/**
+ * Optional configuration for action-creators
+ */
 export interface ActionCreatorOptions<TMeta> {
   meta?: TMeta;
   error?: boolean;
 }
 
+/**
+ * An action-creator is a function capable of creating a type-safe a Flux Standard Action (FSA)
+ */
 export interface ActionCreator<TPayload = void, TMeta = any> {
   (): Action;
   (options?: ActionCreatorOptions<TMeta>): Action<any, TMeta>;
