78c9ddea97281a6a4d7a38773608c6e489d4f7ea
Alan Soares <alanrsoars@gmail.com>
2019-01-15 22:23:12 +1300

    add logo

--- doczrc.js
+++ doczrc.js
@@ -1,18 +1,25 @@
 import { css } from "docz-plugin-css";
 
+const themeConfig = {
+  colors: {
+    primary: "#311E84",
+    secondary: "#FF5447"
+  },
+  logo: {
+    src: "assets/logo-positive.svg",
+    width: 150
+  }
+};
+
 export default {
   title: "re-reduced",
   typescript: true,
-  themeConfig: {
-    colors: {
-      primary: "teal"
-    }
-  },
   menu: ["Home", "Getting Started", "Type Reference", "API Reference"],
   plugins: [
     css({
       preprocessor: "postcss",
       cssmodules: false
     })
-  ]
+  ],
+  themeConfig
 };
