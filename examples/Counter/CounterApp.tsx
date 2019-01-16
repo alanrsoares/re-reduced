b4752e3d2afb0fa19c43921ec136e0049763b0d2
Alan Soares <alanrsoars@gmail.com>
2019-01-16 13:20:19 +1300

    tweak counter demo styles

--- examples/Counter/CounterApp.tsx
+++ examples/Counter/CounterApp.tsx
@@ -9,6 +9,7 @@ import * as selectors from "./selectors";
 const Button = styled.button`
   padding: 5px 10px;
   font-size: 24px;
+  transition: color 0.3s ease-in-out;
 `;
 
 interface Props {
@@ -20,12 +21,17 @@ interface Props {
 
 const Counter = (props: Props) => (
   <div>
-    <Button onClick={() => props.actions.decrement()}>-1</Button>
+    <Button
+      style={{ borderTopLeftRadius: 4, borderBottomLeftRadius: 4 }}
+      onClick={() => props.actions.decrement()}
+    >
+      -1
+    </Button>
     <Button onClick={() => props.actions.adjust(-5)}>-5</Button>
     <Button
       disabled
       style={{
-        color: props.isPositive ? (props.isOdd ? "blue" : "green") : "red",
+        color: props.isPositive ? (props.isOdd ? "#311E84" : "#FF5447") : "red",
         fontWeight: "bold",
         paddingLeft: 5,
         paddingRight: 5,
@@ -35,7 +41,12 @@ const Counter = (props: Props) => (
       {props.count}
     </Button>
     <Button onClick={() => props.actions.adjust(5)}>+5</Button>
-    <Button onClick={() => props.actions.increment()}>+1</Button>
+    <Button
+      style={{ borderTopRightRadius: 4, borderBottomRightRadius: 4 }}
+      onClick={() => props.actions.increment()}
+    >
+      +1
+    </Button>
   </div>
 );
 
