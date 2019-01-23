ff303cdcf38ee5cfb0619de4dbd4ba63dd411737
Alan Soares <alanrsoars@gmail.com>
2019-01-23 16:12:10 +1300

    update doczrc

--- doczrc.js
+++ doczrc.js
@@ -1,19 +1,36 @@
 import { css } from "docz-plugin-css";
 
+const themeConfig = {
+  mode: "dark",
+  colors: {
+    primary: "#FF5447",
+    sidebarBg: "#311E84",
+    background: "#041A28"
+  },
+  logo: {
+    src:
+      "https://raw.githubusercontent.com/alanrsoares/re-reduced/master/docs/assets/logo.png",
+    width: 200
+  },
+  styles: {
+    body: {
+      fontFamily: "Roboto, 'Source Sans Pro', Helvetica, sans-serif"
+    },
+    h1: {
+      fontFamily: "Roboto, 'Source Sans Pro', Helvetica, sans-serif"
+    }
+  }
+};
+
 export default {
   title: "re-reduced",
   typescript: true,
-  hashRouter: true,
-  themeConfig: {
-    colors: {
-      primary: "teal"
-    }
-  },
   menu: ["Home", "Getting Started", "Type Reference", "API Reference"],
   plugins: [
     css({
       preprocessor: "postcss",
       cssmodules: false
     })
-  ]
+  ],
+  themeConfig
 };
