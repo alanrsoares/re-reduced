294b3c4f2622c08d3cd257776ec7701d8183a2da
Alan Soares <alanrsoars@gmail.com>
2019-02-18 09:02:03 +1300

    add a failure handling case to saga tests

--- src/__tests__/sagas.spec.ts
+++ src/__tests__/sagas.spec.ts
@@ -4,22 +4,42 @@ import { apiWorkerFactory } from "../sagas";
 
 describe("Sagas", () => {
   describe("apiWorkerFactory", () => {
-    it("should create a saga that's able to process simple api calls with built-in error handling", () => {
-      const triggerAction = createAsyncAction<void, string[]>("FETCH_FOOS");
-      const mockApiResponse = ["foo", "bar", "baz"];
-      const mockApiCall = () => Promise.resolve(mockApiResponse);
+    describe("creates a saga that's able to process simple api calls with built-in error handling", () => {
+      it("should handle a successful flow", () => {
+        const triggerAction = createAsyncAction<void, string[]>("FETCH_FOOS");
+        const mockApiResponse = ["foo", "bar", "baz"];
+        const mockApiCall = () => Promise.resolve(mockApiResponse);
 
-      const saga = apiWorkerFactory(triggerAction, mockApiCall);
+        const saga = apiWorkerFactory(triggerAction, mockApiCall);
 
-      testSaga(saga, triggerAction())
-        .next()
-        .put(triggerAction.request())
-        .next()
-        .call(mockApiCall)
-        .next(mockApiResponse)
-        .put(triggerAction.success(mockApiResponse))
-        .next()
-        .isDone();
+        testSaga(saga, triggerAction())
+          .next()
+          .put(triggerAction.request())
+          .next()
+          .call(mockApiCall)
+          .next(mockApiResponse)
+          .put(triggerAction.success(mockApiResponse))
+          .next()
+          .isDone();
+      });
+
+      it("should also handle a failure flow", () => {
+        const triggerAction = createAsyncAction<void, string[]>("FETCH_FOOS");
+        const mockError = new Error("something went wrong :(");
+        const mockApiCall = () => Promise.reject(mockError);
+
+        const saga = apiWorkerFactory(triggerAction, mockApiCall);
+
+        testSaga(saga, triggerAction())
+          .next()
+          .put(triggerAction.request())
+          .next()
+          .call(mockApiCall)
+          .throw(mockError)
+          .put(triggerAction.failure(mockError))
+          .next()
+          .isDone();
+      });
     });
   });
 });
