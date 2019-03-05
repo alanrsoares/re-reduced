702ac3ea2e53fc1af8cfa7ede468e4106b729cd1
Alan Soares <alanrsoars@gmail.com>
2019-03-05 17:08:10 +1300

    git rm lib

--- lib/helpers/strings.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const isSnakeCase: (s: string) => boolean;
-export declare const toPascalCase: (s: string) => string;
-export declare const toSnakeCase: (s: string) => string;
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./lib/core";
-export * from "./lib/reducers";
-export * from "./lib/actions";
-export * from "./lib/sagas";
-export * from "./lib/react-redux";
