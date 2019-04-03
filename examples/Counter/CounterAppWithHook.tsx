38e4a43dab1a350942db3f42cae24bf7747e6032
Hu Ze <huze@nzlt1900041.eroad.local>
2019-04-04 10:27:03 +1300

    added hook to bind action and get state

---
+++ examples/Counter/CounterAppWithHook.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+
+import appActions from "./actions";
+import * as selectors from "./selectors";
+import { useActions, useReduxState } from "../../src";
+
+const Button = styled.button`
+  padding: 5px 10px;
+  font-size: 24px;
+  transition: color 0.3s ease-in-out;
+`;
+
+const selectState = {
+  count: selectors.getCounter,
+  isOdd: selectors.getCounterIsOdd,
+  isPositive: selectors.getCounterIsPositive
+};
+
+const Counter = () => {
+  const actions = useActions(appActions);
+  const state = useReduxState(selectState);
+
+  return (
+    <div>
+      <Button
+        style={{ borderTopLeftRadius: 4, borderBottomLeftRadius: 4 }}
+        onClick={() => actions.decrement()}
+      >
+        -1
+      </Button>
+      <Button onClick={() => actions.adjust(-5)}>-5</Button>
+      <Button
+        disabled
+        style={{
+          color: state.isPositive
+            ? state.isOdd
+              ? "#311E84"
+              : "#FF5447"
+            : "red",
+          fontWeight: "bold",
+          paddingLeft: 5,
+          paddingRight: 5,
+          width: 50
+        }}
+      >
+        {state.count}
+      </Button>
+      <Button onClick={() => actions.adjust(5)}>+5</Button>
+      <Button
+        style={{ borderTopRightRadius: 4, borderBottomRightRadius: 4 }}
+        onClick={() => actions.increment()}
+      >
+        +1
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
--- examples/Counter/index.tsx
+++ examples/Counter/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
 import App from "./CounterApp";
+import AppWithHook from "./CounterAppWithHook";
 import reducer from "./reducers";
+import { StoreProvider } from "../../src";
 
 const store = createStore(reducer);
 
 export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <Fragment>
+    {/* use redux connect HOC */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+
+    {/* use redux-react-hook */}
+    <StoreProvider value={store}>
+      <AppWithHook />
+    </StoreProvider>
+  </Fragment>
 );
