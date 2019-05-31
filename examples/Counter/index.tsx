3097b4c6218986d18f136fcb834b77cd1ebff6e0
HU Ze <huze@outlook.sg>
2019-05-31 14:29:20 +1200

    simplified CounterDemo to share the same Provider

--- examples/Counter/index.tsx
+++ examples/Counter/index.tsx
@@ -1,26 +1,23 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
 import App from "./CounterApp";
 import AppWithHook from "./CounterAppWithHook";
 import reducer from "./reducers";
-import { StoreProvider } from "../../src";
 
 const store = createStore(reducer);
 
 export default () => (
-  <Fragment>
-    <span>Using connectWithActions HOC:</span>
+  <Provider store={store}>
     {/* use redux connect HOC */}
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <span>Using connectWithActions HOC:</span>
+    <App />
 
+    <br />
+
+    {/* use hook */}
     <span>Using hooks:</span>
-    {/* use redux-react-hook */}
-    <StoreProvider value={store}>
-      <AppWithHook />
-    </StoreProvider>
-  </Fragment>
+    <AppWithHook />
+  </Provider>
 );
