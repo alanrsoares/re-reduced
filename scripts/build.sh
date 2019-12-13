a58363176c2c4e067d9b77658ec8de8b70ed3e0b
Alan Soares <alanrsoars@gmail.com>
2019-12-13 14:56:47 +1300

    build es modules under es/

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 yarn tsdx build
-yarn tsc -p tsconfig.json --outDir dist/es_temp
+yarn tsc -p tsconfig.json --outDir es_temp
 
-mv dist/es_temp/src dist/es
-rm -rf dist/es_temp
+mv es_temp/src es
+rm -rf es_temp
 
