5619c3c95cf369b2fdd1f5510152c26bd3b4ea52
Alan Soares <alanrsoars@gmail.com>
2020-01-06 09:17:19 +1300

    annotate build_es

--- scripts/build_es.sh
+++ scripts/build_es.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# build esmodules to temp folder
 yarn tsc -p tsconfig.json --outDir es_temp
 
+# clean previous build
 rm -rf es/
+
+# replace with freshly build
 mv es_temp/src es/
 rm -rf es_temp/
 
